96129dd0c25f170d36332e46ceacd56f61f3f57d
Edoardo Negri <edne@gmx.com>
2015-08-03 11:47:17 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name='Hear',
-    version='0.1',
-    packages=['hear'],
-    license='MIT License',
+    name="Hear",
+    author="Edoardo Negri",
+    author_email="edne@gmx.com",
+    url="https://github.com/edne/hear",
+    description="Boilerplate for Jack and PyAudio",
+    long_description="Offers an easy way to read audio input streams, using \
+Jack OR PortAudio backends",
+    version="0.1",
+    packages=["hear"],
+    license="MIT License",
     install_requires=[
         "numpy",
         "pyaudio",
