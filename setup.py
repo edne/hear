43bbc30844c6f4fc81cffaca971c9281651acd36
Edoardo Negri <edne@gmx.com>
2015-08-03 12:12:26 +0200

    long_description from readme

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from distutils.core import setup
 
+readme = "README.md"
+try:
+    from pypandoc import convert
+    long_description = convert(readme, 'rst')
+except ImportError:
+    print("warning: pypandoc not found, could not convert Markdown to RST")
+    long_description = open(readme, 'r').read()
+
 setup(
     name="Hear",
     author="Edoardo Negri",
     author_email="edne@gmx.com",
     url="https://github.com/edne/hear",
     description="Boilerplate for Jack and PyAudio",
-    long_description="Offers an easy way to read audio input streams, using \
-Jack OR PortAudio backends",
+    long_description=long_description,
     version="0.1",
     packages=["hear"],
     license="MIT License",
